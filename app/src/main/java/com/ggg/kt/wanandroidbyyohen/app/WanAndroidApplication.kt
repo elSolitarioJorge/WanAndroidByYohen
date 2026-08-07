8813297a71bf4fdaf089d9649d8ff5017d69067f
elSolitarioJorge <Liuhou77@outlook.com>
2026-08-07 19:13:12 +0800

    feat: 在 Debug 模式下启用 StrictMode 以检测线程违规操作

--- app/src/main/java/com/ggg/kt/wanandroidbyyohen/app/WanAndroidApplication.kt
+++ app/src/main/java/com/ggg/kt/wanandroidbyyohen/app/WanAndroidApplication.kt
@@ -1,11 +1,32 @@
 package com.ggg.kt.wanandroidbyyohen.app
 
 import android.app.Application
+import android.content.pm.ApplicationInfo
+import android.os.StrictMode
 
 class WanAndroidApplication : Application() {
 
     override fun onCreate() {
         super.onCreate()
         AppContext.init(this)
+
+        if (isDebuggable()) {
+            enableStrictMode()
+        }
+    }
+
+    private fun isDebuggable() : Boolean {
+        return applicationInfo.flags and ApplicationInfo.FLAG_DEBUGGABLE != 0
+    }
+
+    private fun enableStrictMode() {
+        StrictMode.setThreadPolicy(
+            StrictMode.ThreadPolicy.Builder()
+                .detectDiskReads()
+                .detectDiskWrites()
+                .detectNetwork()
+                .penaltyLog()
+                .build()
+        )
     }
 }
